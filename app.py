c5befd81ee60a603496e66373802cb050029f4f3
technickal1 <31661206+technickal1@users.noreply.github.com>
2023-03-07 17:41:53 -0500

    add a button

--- app.py
+++ app.py
@@ -10,16 +10,36 @@ st.header("OpenAI Chat API")
 input_key = "my_input" # Define a unique key for the text input
 input_value = "" # Define a placeholder value for the input
 
-# Define a function to execute when the Enter key is pressed
-def on_enter_pressed():
+# Define a function to execute when the button is clicked
+def on_button_click():
     # Get the current value of the text input
     input_value = st.session_state[input_key]
 
-    # Do something with the input value
-    st.write("You entered:", input_value)
+    # a ChatCompletion request
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[
+            {'role': 'user', 'content': input_value}
+        ],
+        stream=True  # streamed
+    )
+
+    # create variables to collect the stream of chunks
+    collected_chunks = []
+    collected_messages = []
+    for chunk in response:
+        collected_chunks.append(chunk)  # save the event response
+        if 'choices' in chunk and chunk['choices']:  # check if 'choices' key exists and is not empty
+            chunk_message = chunk['choices'][0]['delta']  # extract the message
+            if chunk_message:  # check if message is not empty
+                collected_messages.append(chunk_message)  # save the message
+        stream_content = ''.join([s.get('content', '') for s in collected_messages if s])  # add check for empty strings
+        # print(stream_content) # use for troubleshooting
+        stream_content = stream_content.strip()  # remove leading/trailing whitespace
+        res_box.markdown(stream_content)
 
 # Create the text input box
-user_input = st.text_input("Prompt", placeholder="Ask anything and press Enter", key=input_key, on_change=on_enter_pressed)
+user_input = st.text_input("Prompt", placeholder="Ask anything and press Enter", key=input_key)
 
 # Store the current value of the text input in session state
 if input_key not in st.session_state:
@@ -27,30 +47,12 @@ if input_key not in st.session_state:
 else:
     input_value = st.session_state[input_key]
 
+# Create a button widget and define its behavior when clicked
+button = st.button("Send")
+if button:
+    on_button_click()
+
 res_box = st.empty()
-
-# a ChatCompletion request
-response = openai.ChatCompletion.create(
-    model='gpt-3.5-turbo',
-    messages=[
-        {'role': 'user', 'content': user_input}
-    ],
-    stream=True  # streamed
-)
-
-# create variables to collect the stream of chunks
-collected_chunks = []
-collected_messages = []
-for chunk in response:
-    collected_chunks.append(chunk)  # save the event response
-    if 'choices' in chunk and chunk['choices']:  # check if 'choices' key exists and is not empty
-        chunk_message = chunk['choices'][0]['delta']  # extract the message
-        if chunk_message:  # check if message is not empty
-            collected_messages.append(chunk_message)  # save the message
-    stream_content = ''.join([s.get('content', '') for s in collected_messages if s])  # add check for empty strings
-    # print(stream_content) # use for troubleshooting
-    stream_content = stream_content.strip()  # remove leading/trailing whitespace
-    res_box.markdown(stream_content)
 
 # show in the text box
 # res_box.markdown(f'*{stream_content}*')
