d3983fd71ff95f0b18fc8990e524d5c59a5b4865
disruptive-ai <125858375+disruptive-ai@users.noreply.github.com>
2023-03-07 10:42:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,40 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import openai
 import streamlit as st
+from streamlit_pills import pills
 
-"""
-# Welcome to Streamlit!
+openai.api_key = st.secrets['api_secret']
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.subheader("AI Assistant : Streamlit + OpenAI: `stream` *argument*")
+selected = pills("", ["NO Streaming", "Streaming"], ["🎈", "🌈"])
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+user_input = st.text_input("You: ",placeholder = "Ask me anything ...", key="input")
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if st.button("Submit", type="primary"):
+    st.markdown("----")
+    res_box = st.empty()
+    if selected == "Streaming":
+        report = []
+        for resp in openai.Completion.create(model='text-davinci-003',
+                                            prompt=user_input,
+                                            max_tokens=120, 
+                                            temperature = 0.5,
+                                            stream = True):
+            # join method to concatenate the elements of the list 
+            # into a single string, 
+            # then strip out any empty strings
+            report.append(resp.choices[0].text)
+            result = "".join(report).strip()
+            result = result.replace("\n", "")        
+            res_box.markdown(f'*{result}*') 
+            
+    else:
+        completions = openai.Completion.create(model='text-davinci-003',
+                                            prompt=user_input,
+                                            max_tokens=120, 
+                                            temperature = 0.5,
+                                            stream = False)
+        result = completions.choices[0].text
+        
+        res_box.write(result)
+st.markdown("----")
